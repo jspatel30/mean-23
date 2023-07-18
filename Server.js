4bc29c86a040bcb1d68a4e831c16fae65ea73cb0
jspatel30 <jspateldeveloper@gmail.com>
2023-07-18 12:32:28 +0530

    update

--- Server.js
+++ Server.js
@@ -6,6 +6,9 @@ const CategoryRoute = require("./app/src/routes/category.routes")
 const authMiddleware = require("./app/src/middleware/auth.middleware")
 const publicRoute = require("./app/src/routes/public.Routes")
 
+const sessionControllerDB = require("./app/src/controller/signup_loginController")
+
+
 //middleware
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -17,6 +20,10 @@ app.use("/admin",authMiddleware,CategoryRoute)
 
 //public
 app.use("/public",publicRoute)
+
+app.post("/signup",sessionControllerDB.signup)
+app.get("/getAllUser",sessionControllerDB.getAllUser)
+app.post("/login",sessionControllerDB.login)
 
 app.listen(9999,function(){
     console.log("Server Started at 9999")
--- app/src/controller/signup_loginController.js
+++ app/src/controller/signup_loginController.js
@@ -1,6 +1,7 @@
 const UserModel = require("../model/userModel")
 const jwt = require("jsonwebtoken")
 const {SEC_KEY} = process.env
+const bcrypt = require("bcrypt")
 
 //signup
 module.exports.signup = async function(req,res){
@@ -10,21 +11,25 @@ module.exports.signup = async function(req,res){
     //     email:req.body.email,
     //     password:req.body.password
     // })
+    let password = req.body.password
+    let encryptedPassoword = bcrypt.hashSync(password,10)
+    console.log("enc pass",encryptedPassoword)
+    req.body.password = encryptedPassoword
+    
     let user = new UserModel(req.body)
     let data = await user.save()
-
     res.json({"msg":"Signup done",data:data,"rescode":200})     
-}
+}   
 
 //login
 module.exports.login = async function(req,res){
     let email = req.body.email
     let password = req.body.password
-    
+    console.log(password)
     let user = await UserModel.findOne({email:email})
     
-    
-    if(user && user.password == password)
+    console.log(user.password)
+    if(user && bcrypt.compareSync(password,user.password))
     { 
             let token = jwt.sign({"email":user.email,"userId":user._id,"role":"user"},SEC_KEY,{expiresIn:"7d"})
             res.json({"msg":"Login Successfully","data":user,rescode:200,token:token})
@@ -33,4 +38,12 @@ module.exports.login = async function(req,res){
     {
         res.json({"msg":"Login Failed","data":"User Not Available",rescode:-9})
     }
+}
+
+module.exports.getAllUser = function(req,res){
+    UserModel.find().then(data=>{
+        res.json({data:data,"msg":"USer Retrieved",rescode:200})
+    }).catch((err)=>{
+        res.json({data:data,"msg":"USer Retrieved",rescode:200})
+    })
 }
