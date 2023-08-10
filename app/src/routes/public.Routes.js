738fb08911439d059dcab361b0da5ae3bb6e3da0
jspatel30 <jspateldeveloper@gmail.com>
2023-08-10 11:49:58 +0530

    publicroutes

--- app/src/routes/public.Routes.js
+++ app/src/routes/public.Routes.js
@@ -6,5 +6,5 @@ const signupController = require("../controller/signup_loginController")
 route.post("/signupDb",signupController.signup)
 route.post("/login",signupController.login)
 route.get("/getAllUsers",signupController.getAllUser)
-route.delete("/delete/:userId",signupController.deleteUserById)
+route.delete("/deleteUserById/:userId",signupController.deleteUserById)
 module.exports = route
